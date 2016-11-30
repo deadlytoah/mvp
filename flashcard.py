b89bcdab2770c991c527df85e8efff4a47d283b7
Chris Shin <heesuk@gmail.com>
2016-12-01 02:13:43 +0900

    Warn about empty database in flash card screen.

--- flashcard.py
+++ flashcard.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 """Displays the flash card of the Bible verses."""
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import uic
 from screen import toggle_screen
 
@@ -14,8 +14,34 @@ class FlashCardForm(QtWidgets.QDialog):
         self.gui = uic.loadUi("flashcard.ui")
         self.gui.action_enter_verses.triggered.connect(enter_verses)
 
+        self.canvas = FlashCardCanvas()
+        self.gui.setCentralWidget(self.canvas)
+
         global window
         window = self
+
+class FlashCardCanvas(QtWidgets.QWidget):
+    def __init__(self):
+        super(FlashCardCanvas, self).__init__()
+
+    def paintEvent(self, event):
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        self._warn_empty_database(qp, event)
+        qp.end()
+
+    def _warn_empty_database(self, qp, event):
+        title = 'Nothing in the database yet.'
+        message = 'Please enter some verses first.'
+
+        qp.setPen(QtGui.QColor('black'))
+        qp.setFont(QtGui.QFont('Serif', 20, QtGui.QFont.Black))
+        rect = event.rect()
+        rect.moveTop(-30)
+        qp.drawText(rect, QtCore.Qt.AlignCenter, title)
+        qp.setPen(QtGui.QColor('gray'))
+        qp.setFont(QtGui.QFont('Serif', 12))
+        qp.drawText(event.rect(), QtCore.Qt.AlignCenter, message)
 
 def enter_verses():
     """switch to the verse entry screen"""
