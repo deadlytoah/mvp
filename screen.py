ab7356862da75828c182ecb5f4cb50edf0a341ed
Chris Shin <heesuk@gmail.com>
2016-12-01 00:33:54 +0900

    Completely destroy the old form when switching.

--- screen.py
+++ screen.py
@@ -1,19 +1,30 @@
 INIT_FORM_INDEX = 0
 
-def init_screens(form_types):
+def init_screens(all_form_types):
     """initialises the screens created using the given form types."""
-    global forms
-    forms = [form_type() for form_type in form_types]
-
     global current_form_index
     current_form_index = INIT_FORM_INDEX
-    forms[current_form_index].gui.show()
+
+    global form_types
+    form_types = all_form_types
+
+    global form
+    form = form_types[current_form_index]()
+    form.gui.show()
 
 def _switch_to_screen(form_index):
     global current_form_index
-    forms[current_form_index].gui.hide()
     current_form_index = form_index
-    forms[current_form_index].gui.show()
+
+    global form
+    form.gui.hide()
+    form.gui.close()
+    form.gui.deleteLater()
+    form.close()
+    form.deleteLater()
+
+    form = form_types[current_form_index]()
+    form.gui.show()
 
 def toggle_screen():
     """toggle between current and last screen...  But since I know there
