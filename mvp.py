47b4b35c408b9325525f87aa082b84bb76470974
Chris Shin <heesuk@gmail.com>
2016-12-10 13:44:28 +0900

    make flash card screen to be the initial screen.

--- mvp.py
+++ mvp.py
@@ -7,7 +7,7 @@ from screen import init_screens
 from dataentry import DataEntryForm
 from flashcard import FlashCardForm
 
-FORM_TYPES = [DataEntryForm, FlashCardForm]
+FORM_TYPES = [FlashCardForm, DataEntryForm]
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
