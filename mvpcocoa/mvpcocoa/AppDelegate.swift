ea06bdc8d4652e12bb068315e99700b539843df5
Chris Shin <heesuk@gmail.com>
2018-08-02 21:10:10 +0900

    Add mvpcocoa

---
+++ mvpcocoa/mvpcocoa/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mvpcocoa
+//
+//  Created by Chris Heesuk Shin on 01/08/2018.
+//  Copyright © 2018 Hee Suk Shin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
