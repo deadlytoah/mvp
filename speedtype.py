dd1af901219b036549f724532485ab64c87d3f14
Chris Shin <heesuk@gmail.com>
2018-08-20 13:21:41 +0900

    Remove stack and jump to dialog.
    A lot of code were carried over from flashcard.py and are no longer
    needed.

--- speedtype.py
+++ speedtype.py
@@ -48,20 +48,12 @@ class SpeedTypeForm:
         global window
         window = self
 
-        self.jump_to_dialog = uic.loadUi('jump-to.ui')
-        self.jump_to_dialog.lineedit_jump_to.textEdited.connect(_peek)
-        self.jump_to_dialog.finished.connect(_conclude_jump)
-        self.jump_to_dialog.move(0, 0)
-
         self.gui = uic.loadUi("speedtype.ui")
         self.gui.action_enter_verses.triggered.connect(_view_enter_verses)
         self.gui.action_view_flash_cards.triggered.connect(_view_flash_cards)
         self.gui.action_edit_session.triggered.connect(_edit_session)
         self.gui.action_debug_inspect_database.triggered.connect(_debug_view_db)
-        self.gui.action_debug_layout_engine.triggered.connect(_debug_layout_engine)
         self.gui.action_debug_sentences.triggered.connect(_debug_sentences)
-        self.gui.action_display_graph.triggered.connect(_debug_display_graph)
-        self.gui.action_jump_to.triggered.connect(_prepare_jump)
 
         self.gui.difficulty_level.valueChanged.connect(
             self._change_difficulty_level)
@@ -79,7 +71,6 @@ class SpeedTypeForm:
         self.verse_table.create_manager()
         self.verse_table.verify()
         self.verse_table.service()
-        records = self.verse_table.select_all()
 
         # Load or initialise the session
         try:
@@ -106,18 +97,7 @@ class SpeedTypeForm:
             sess['range'] = { 'start': start, 'end': end }
 
         self.session = sess
-
-        # Stack is used to help implementing input session.  The top
-        # of the stack contains the currently display flash card, and
-        # the very bottom is what we will revert to if the input
-        # session fails.
-        self.stack = []
-
-        if len([r for r in records if r['deleted'] == '0']) == 0:
-            self.canvas.set_empty_database()
-        else:
-            self.stack.append(_address_from_key(Key.from_str(records[0]['key'])))
-            _display_by_address(self.stack[-1])
+        _start_session()
 
     def _change_difficulty_level(self):
         """user changed the difficulty level by manipulating the slider"""
@@ -335,13 +315,7 @@ class SpeedTypeCanvas(QtWidgets.QWidget):
             ch['visible'] = True
 
     def keyPressEvent(self, event):
-        if event.key() == Qt.Qt.Key_Up:
-            window.stack[-1].move_up()
-            _display_by_address(window.stack[-1])
-        elif event.key() == Qt.Qt.Key_Down:
-            window.stack[-1].move_down()
-            _display_by_address(window.stack[-1])
-        elif event.key() == Qt.Qt.Key_Backspace:
+        if event.key() == Qt.Qt.Key_Backspace:
             if self.caret > 0:
                 self.caret = self.caret - 1
                 ch = self.buf[self.caret]
@@ -534,36 +508,6 @@ def _start_session():
     key = Key(loc['book'], str(loc['chapter']), str(1))
     _display_by_address(_address_from_key(key))
 
-def _peek():
-    dest = window.jump_to_dialog.lineedit_jump_to.text()
-    key = Key.from_str(dest)
-    if key.chapter == None:
-        key.chapter = '1'
-    if key.verse == None:
-        key.verse = '1'
-
-    records = window.verse_table.select_all()
-    matches = []
-    for rec in records:
-        reckey = Key.from_str(rec['key'])
-        if reckey.book.lower().startswith(key.book.lower()) and \
-           reckey.chapter == key.chapter and \
-           reckey.verse == key.verse:
-            matches.append(reckey)
-
-    if len(matches) > 0:
-        window.jump_to_dialog.button_box.button(QtWidgets.QDialogButtonBox.Ok).setEnabled(True)
-        window.stack.append(_address_from_key(matches[0]))
-
-        _display_by_address(window.stack[-1])
-        window.canvas.set_title('Peeking at: {0} (enter – jump, esc – cancel)'.format(window.canvas.title))
-    else:
-        window.jump_to_dialog.button_box.button(QtWidgets.QDialogButtonBox.Ok).setEnabled(False)
-        window.canvas.set_title('No Matches')
-        window.canvas.set_text('Please enter a valid book abbreviation followed by chapter number.')
-
-    window.canvas.update()
-
 def _address_from_key(key):
     records = window.verse_table.select_all()
     sentences, lookup = sentences_cons2(records, key.book, key.chapter)
@@ -579,23 +523,6 @@ def _display_by_address(address):
     window.canvas.set_title(label + ' (' + config.translation.upper() + ')')
     window.canvas.set_text(sentence['text'])
     window.canvas.update()
-
-def _conclude_jump(result):
-    if result == QtWidgets.QDialog.Accepted:
-        address = window.stack[-1]
-    else:
-        address = window.stack[0]
-
-    window.stack = [address]
-    window.jump_to_dialog.lineedit_jump_to.clear()
-
-    _display_by_address(address)
-
-def _prepare_jump():
-    if len(window.stack) > 0:
-        window.jump_to_dialog.show()
-    else:
-        QtWidgets.QMessageBox.warning(window.gui, 'mvp – Jump to', 'Unable to jump right now.')
 
 def sentences_cons2(records, book, chapter):
     sentences = []
@@ -718,72 +645,28 @@ def _debug_view_db():
     from dbgviewdb import DbgViewDb
     DbgViewDb().gui.show()
 
-def _debug_layout_engine():
-    if len(window.stack) > 0:
-        from dbglayout import DbgLayout
-        from simplelayout import SimpleLayout
-        from graphlayout import GraphLayout
-
-        address = window.stack[0]
-
-        records = window.verse_table.select_all()
-        sentences, lookup = sentences_cons2(records, address.book, address.chapter)
-        sentence = sentences[address.sentence]
-
-        text = sentence['text']
-
-        dbglayout = DbgLayout(window.gui)
-        dbglayout.set_text(text)
-        dbglayout.add_layout_engine(SimpleLayout())
-        dbglayout.add_layout_engine(GraphLayout())
-        dbglayout.show()
-    else:
-        QtWidgets.QMessageBox.warning(window.gui,
-                                      'Debug – Layout Engines',
-                                      'Unable to draw layouts')
-
 def _debug_sentences():
-    if len(window.stack) > 0:
-        from dbgsentences import DbgSentences
-        address = window.stack[0]
-        records = window.verse_table.select_all()
-        (sentences, lookup) = sentences_cons2(records, address.book, address.chapter)
-        info = DbgSentences()
-        info.gui.label_source.setText('Sentences constructed from: '
-                                      + ' '.join([address.book, address.chapter]))
+    from dbgsentences import DbgSentences
 
-        sentencesstr = ''
-        for sentence in sentences:
-            sentencesstr += ' - ' \
-                + sentence_make_label(sentence, address.book, address.chapter) \
-                + ': ' + sentence['text'] + '\n'
-        info.gui.textedit_sentences.setPlainText(sentencesstr)
+    global window
+    loc = window.session['range']['start']
+    address = _address_from_key(Key(loc['book'], loc['chapter'], str(1)))
+    records = window.verse_table.select_all()
+    (sentences, lookup) = sentences_cons2(records, address.book, address.chapter)
+    info = DbgSentences()
+    info.gui.label_source.setText('Sentences constructed from: '
+                                  + ' '.join([address.book, address.chapter]))
 
-        lookupstr = ''
-        for i, sentid in enumerate(lookup):
-            lookupstr += 'Verse {0}: {1}\n'.format(1 + i, sentences[sentid]['text'])
-        info.gui.textedit_lookup.setPlainText(lookupstr)
+    sentencesstr = ''
+    for sentence in sentences:
+        sentencesstr += ' - ' \
+            + sentence_make_label(sentence, address.book, address.chapter) \
+            + ': ' + sentence['text'] + '\n'
+    info.gui.textedit_sentences.setPlainText(sentencesstr)
 
-        info.gui.show()
-    else:
-        QtWidgets.QMessageBox.warning(window.gui, 'Debug – Sentences', 'Unable to show sentences')
+    lookupstr = ''
+    for i, sentid in enumerate(lookup):
+        lookupstr += 'Verse {0}: {1}\n'.format(1 + i, sentences[sentid]['text'])
+    info.gui.textedit_lookup.setPlainText(lookupstr)
 
-def _debug_display_graph():
-    from dbggraph import DbgGraph
-
-    if len(window.stack) > 0:
-        address = window.stack[0]
-
-        records = window.verse_table.select_all()
-        sentences, lookup = sentences_cons2(records, address.book, address.chapter)
-        sentence = sentences[address.sentence]
-
-        text = sentence['text']
-
-        dbggraph = DbgGraph()
-        dbggraph.set_text(text)
-        dbggraph.gui.show()
-    else:
-        QtWidgets.QMessageBox.warning(window.gui,
-                                      'Debug – Display Graph',
-                                      'Unable to display graph')
+    info.gui.show()
