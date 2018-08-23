ab5faefcc85b3f62e2e1518ef29c3e4b07e04921
Chris Shin <heesuk@gmail.com>
2018-08-23 17:17:16 +0900

    Move sentence related functions in a module.

---
+++ sentence.py
@@ -0,0 +1,119 @@
+import config
+from key import Key
+
+def sentences_cons2(records, book, chapter):
+    sentences = []
+    lookup = []
+    keyprefix = ' '.join([book, chapter])
+
+    matches = []
+    for rec in records:
+        key = Key.from_str(rec['key'])
+        if key.book == book and \
+           key.chapter == chapter and \
+           rec['deleted'] == '0':
+            matches.append({
+                'key': key,
+                'record': rec
+            })
+
+    verseseq = [match['record']['text']
+                for match in sorted(matches,
+                                    key=lambda m: int(m['key'].verse))]
+
+    # step 1 split verses into a queue of partial or whole verses.
+    indiciesseq = [sorted(_find_all_delimiters(text, config.SENTENCE_DELIMITERS))
+                   for text in verseseq]
+    splitsseq = []
+    for i, text in enumerate(verseseq):
+        splitsseq.append(_split_verse(i + 1, text, indiciesseq[i]))
+
+    # flatten list of lists
+    queue = [split for splits in splitsseq for split in splits]
+
+    # step 2 process the queue to produce sentences and look up table.
+    sentence = {
+        'text': '',
+        'textseq': [],
+        'sources': []
+    }
+    for split in queue:
+        sentence['textseq'].append(split['text'])
+        sentence['sources'].append({
+            'verse': split['origin'],
+            'partid': split['partid'],
+            'iswhole': split['iswhole']
+        })
+
+        if len(lookup) < split['origin']:
+            lookup.append(len(sentences))
+
+        if split['isfinal']:
+            sentence['text'] = ' '.join(sentence['textseq'])
+            sentences.append(sentence)
+            sentence = {
+                'text': '',
+                'textseq': [],
+                'sources': []
+            }
+
+    return (sentences, lookup)
+
+def sentences_find_by_verseno(sentences, lookup, verseno):
+    i = int(verseno) - 1
+    sentid = lookup[i]
+    return sentences[sentid]
+
+def sentences_index_by_verseno(sentences, lookup, verseno):
+    i = int(verseno) - 1
+    return lookup[i]
+
+def sentence_make_label(sentence, book, chapter):
+    srclist = []
+    sources = sentence['sources']
+    for source in sources:
+        if source['iswhole']:
+            srclist.append(str(source['verse']))
+        else:
+            srclist.append(str(source['verse']) + chr(ord('a') + source['partid']))
+    return book + ' ' + chapter + ':' + ', '.join(srclist)
+
+def _split_verse(verse, text, indices):
+    split = []
+    start = 0
+
+    # use of lstrip() is due to a whitespace that follows a
+    # punctuation.
+    for j, index in enumerate(indices):
+        split.append({
+            'text': text[start:index + 1].lstrip(),
+            'partid': j,
+            'origin': verse,
+            'isfinal': True,
+            'iswhole': False
+        })
+        start = index + 1
+
+    if len(text[start:]) > 0:
+        split.append({
+            'text': text[start:].lstrip(),
+            'partid': len(split),
+            'origin': verse,
+            'isfinal': False,
+            'iswhole': False
+        })
+
+    if len(split) == 1:
+        split[0]['iswhole'] = True
+
+    return split
+
+def _find_all_delimiters(text, delimiters):
+    indicies = []
+    for delim in delimiters:
+        index = text.find(delim, 0)
+        while index >= 0:
+            indicies.append(index)
+            start = index
+            index = text.find(delim, start + 1)
+    return indicies
--- speedtype.py
+++ speedtype.py
@@ -14,6 +14,7 @@ from PyQt5 import uic
 from address import Address
 from key import Key
 from random import randrange
+from sentence import sentences_cons2, sentences_index_by_verseno
 from sdb import Sdb
 from graphlayout import GraphLayout
 
@@ -633,123 +634,6 @@ def _view_enter_verses():
     """switch to the verse entry screen"""
     screen.switch_to(screen.DATA_ENTRY_INDEX)
 
-def sentences_cons2(records, book, chapter):
-    sentences = []
-    lookup = []
-    keyprefix = ' '.join([book, chapter])
-
-    matches = []
-    for rec in records:
-        key = Key.from_str(rec['key'])
-        if key.book == book and \
-           key.chapter == chapter and \
-           rec['deleted'] == '0':
-            matches.append({
-                'key': key,
-                'record': rec
-            })
-
-    verseseq = [match['record']['text']
-                for match in sorted(matches,
-                                    key=lambda m: int(m['key'].verse))]
-
-    # step 1 split verses into a queue of partial or whole verses.
-    indiciesseq = [sorted(_find_all_delimiters(text, config.SENTENCE_DELIMITERS))
-                   for text in verseseq]
-    splitsseq = []
-    for i, text in enumerate(verseseq):
-        splitsseq.append(_split_verse(i + 1, text, indiciesseq[i]))
-
-    # flatten list of lists
-    queue = [split for splits in splitsseq for split in splits]
-
-    # step 2 process the queue to produce sentences and look up table.
-    sentence = {
-        'text': '',
-        'textseq': [],
-        'sources': []
-    }
-    for split in queue:
-        sentence['textseq'].append(split['text'])
-        sentence['sources'].append({
-            'verse': split['origin'],
-            'partid': split['partid'],
-            'iswhole': split['iswhole']
-        })
-
-        if len(lookup) < split['origin']:
-            lookup.append(len(sentences))
-
-        if split['isfinal']:
-            sentence['text'] = ' '.join(sentence['textseq'])
-            sentences.append(sentence)
-            sentence = {
-                'text': '',
-                'textseq': [],
-                'sources': []
-            }
-
-    return (sentences, lookup)
-
-def sentences_find_by_verseno(sentences, lookup, verseno):
-    i = int(verseno) - 1
-    sentid = lookup[i]
-    return sentences[sentid]
-
-def sentences_index_by_verseno(sentences, lookup, verseno):
-    i = int(verseno) - 1
-    return lookup[i]
-
-def sentence_make_label(sentence, book, chapter):
-    srclist = []
-    sources = sentence['sources']
-    for source in sources:
-        if source['iswhole']:
-            srclist.append(str(source['verse']))
-        else:
-            srclist.append(str(source['verse']) + chr(ord('a') + source['partid']))
-    return book + ' ' + chapter + ':' + ', '.join(srclist)
-
-def _split_verse(verse, text, indices):
-    split = []
-    start = 0
-
-    # use of lstrip() is due to a whitespace that follows a
-    # punctuation.
-    for j, index in enumerate(indices):
-        split.append({
-            'text': text[start:index + 1].lstrip(),
-            'partid': j,
-            'origin': verse,
-            'isfinal': True,
-            'iswhole': False
-        })
-        start = index + 1
-
-    if len(text[start:]) > 0:
-        split.append({
-            'text': text[start:].lstrip(),
-            'partid': len(split),
-            'origin': verse,
-            'isfinal': False,
-            'iswhole': False
-        })
-
-    if len(split) == 1:
-        split[0]['iswhole'] = True
-
-    return split
-
-def _find_all_delimiters(text, delimiters):
-    indicies = []
-    for delim in delimiters:
-        index = text.find(delim, 0)
-        while index >= 0:
-            indicies.append(index)
-            start = index
-            index = text.find(delim, start + 1)
-    return indicies
-
 def _address_from_key(key):
     records = window.verse_table.select_all()
     sentences, lookup = sentences_cons2(records, key.book, key.chapter)
@@ -761,10 +645,11 @@ def _debug_view_db():
     DbgViewDb().gui.show()
 
 def _debug_sentences():
+    from sentence import sentence_make_label
     from dbgsentences import DbgSentences
 
     global window
-    loc = window.session['range']['start']
+    loc = window.canvas.session['range']['start']
     address = _address_from_key(Key(loc['book'], loc['chapter'], str(1)))
     records = window.verse_table.select_all()
     (sentences, lookup) = sentences_cons2(records, address.book, address.chapter)
