8d5f6741465fe0580b141813205ab8f79eff56e1
Chris Shin <heesuk@gmail.com>
2018-09-04 22:01:05 +0900

    [core] Define data structures for hiding words.
    Using pseudo-random number to hiding words is not always helpful.
    Hidden words are often not evenly distributed, making the easy
    difficulty levels much harder than desired.
    
    This change is the first step to determine which words to hide by
    based on some metrics.

--- mvpcore/src/level.rs
+++ mvpcore/src/level.rs
@@ -16,3 +16,103 @@ impl From<u8> for Level {
         }
     }
 }
+
+static PRONOUNS: &[&str] = &[
+    "i",
+    "we",
+    "they",
+    "she",
+    "he",
+    "you",
+    "my",
+    "mine",
+    "our",
+    "ours",
+    "their",
+    "theirs",
+    "her",
+    "hers",
+    "his",
+    "hers",
+    "your",
+    "yours",
+    "me",
+    "us",
+    "them",
+    "him",
+    "while",
+    "when",
+    "where",
+    "how",
+    "who",
+    "whom",
+    "why",
+    "whenever",
+    "wherever",
+    "however",
+    "whoever",
+    "whomever",
+    "whosoever",
+];
+
+static ARTICLES: &[&str] = &[
+    "to",
+    "for",
+    "in",
+    "on",
+    "at",
+    "toward",
+    "towards",
+    "afterward",
+    "afterwards",
+    "over",
+    "under",
+    "across",
+    "beyond",
+    "before",
+    "after",
+    "unto",
+    "onto",
+    "inside",
+    "through",
+    "throughout",
+    "therefore",
+    "moreover",
+    "if",
+    "but",
+    "and",
+    "or",
+    "hence",
+    "thus",
+];
+
+static punctuations: &[char] = &['.', ',', ':', ';', '[', ']'];
+
+#[derive(Clone)]
+#[repr(C)]
+enum Kind {
+    Pronoun,
+    Article,
+}
+
+#[derive(Clone)]
+#[repr(C)]
+struct Word {
+    pub value: String,
+    pub hidden: bool,
+    pub kind: Kind,
+}
+
+#[repr(C)]
+struct Permutation {
+    pub words: Vec<Word>,
+
+    /// How dispersed are the hidden words?
+    pub dispersion: f64,
+
+    /// How many pronouns are hidden?
+    pub pronouns: f64,
+
+    /// How many articles are hidden?
+    pub articles: f64,
+}
