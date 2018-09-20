525ebef051dacfb76000898340f1e96d9c2a7a95
Chris Shin <heesuk@gmail.com>
2018-09-20 12:35:05 +0900

    Enable typing using mock models.
    This shows me that the models I used for the Python app would work
    on the Mac app as well.

--- mvpcocoa/mvpcocoa/SpeedTypeView.swift
+++ mvpcocoa/mvpcocoa/SpeedTypeView.swift
@@ -9,44 +9,13 @@
 import Cocoa
 
 class SpeedTypeView: NSTextView {
-    var caret_position: Int {
-        get {
-            let range = self.selectedRanges[0].rangeValue
-            return range.location + range.length
-        }
-        set {
-            self.selectedRange = NSRange(location: newValue, length: 0)
-        }
-    }
-
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
 
         // Drawing code here.
     }
 
-    // Allow view to receive keypress (remove the purr sound)
-    override var acceptsFirstResponder : Bool {
-        return true
-    }
-
-    // Override the NSView keydown func to read keycode of pressed key
-    /*override func keyDown(with theEvent: NSEvent) {
-        if theEvent.characters != nil {
-            for ch in theEvent.characters! {
-                let caret = self.caret_position
-                self.textStorage?.mutableString.replaceCharacters(in: NSRange(location: caret, length: 1), with: String(ch))
-                self.textStorage?.addAttribute(NSAttributedStringKey.foregroundColor, value: NSColor.black, range: NSRange(location: caret, length: 1))
-            }
-        }
-    }*/
-
-    override func insertText(_ string: Any, replacementRange: NSRange) {
-        let s: String = string as! String
-        for ch in s {
-            let caret = self.caret_position
-            self.textStorage?.mutableString.replaceCharacters(in: NSRange(location: caret, length: 1), with: String(ch))
-            self.textStorage?.addAttribute(NSAttributedStringKey.foregroundColor, value: NSColor.black, range: NSRange(location: caret, length: 1))
-        }
+    override func keyDown(with theEvent: NSEvent) {
+        nextResponder?.keyDown(with: theEvent)
     }
 }
