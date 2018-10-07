a915e2e3219911326fac16bc213d30512e362557
Chris Shin <heesuk@gmail.com>
2018-10-08 03:02:11 +0900

    Cover cases where terminating punctuation is followed by other punctuations.
    This fixes cases where characters like ) or " opened up a line.  This
    looked very awkward.

--- mvpcore/src/model/strong/sentence.rs
+++ mvpcore/src/model/strong/sentence.rs
@@ -4,7 +4,81 @@ use std::ffi::CStr;
 use std::mem::replace;
 use std::slice;
 
-const SENTENCE_DELIMITERS: &str = ".:;?!";
+const TERMINATING_PUNCTS: &str = ".:;?!";
+const NON_TERMINATING_PUNCTS: &str = r#"()[]<>"'"#;
+
+#[derive(Debug)]
+enum PunctuationKind {
+    Terminating {
+        // Wait, There is More
+        //
+        // Non-terminating punctuations that follow a terminating one
+        // belong in the sentence it is terminating.
+        precedes_punctuation: bool,
+    },
+    NonTerminating {
+        // End of Sentence
+        //
+        // Preceding whitespace means we will stop looking (expecting)
+        // another punctuation after this.  Thus we conclude this
+        // sentence.
+        precedes_whitespace: bool,
+
+        // Indicates this is the last character in the string.
+        end_of_string: bool,
+    },
+}
+
+#[derive(Debug)]
+struct Punctuation {
+    index: usize,
+    punctuation: char, // for debugging
+    kind: PunctuationKind,
+}
+
+impl Punctuation {
+    fn is_terminating(&self) -> bool {
+        if let PunctuationKind::Terminating { .. } = self.kind {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn from_char(index: usize, ch: char, next_char: Option<char>) -> Option<Self> {
+        if TERMINATING_PUNCTS.contains(ch) {
+            Some(Punctuation {
+                index,
+                punctuation: ch,
+                kind: PunctuationKind::Terminating {
+                    precedes_punctuation: next_char.is_some()
+                        && (TERMINATING_PUNCTS.contains(next_char.unwrap())
+                            || NON_TERMINATING_PUNCTS.contains(next_char.unwrap())),
+                },
+            })
+        } else if NON_TERMINATING_PUNCTS.contains(ch) {
+            Some(Punctuation {
+                index,
+                punctuation: ch,
+                kind: PunctuationKind::NonTerminating {
+                    precedes_whitespace: next_char.is_some() && next_char.unwrap().is_whitespace(),
+                    end_of_string: next_char.is_none(),
+                },
+            })
+        } else {
+            None
+        }
+    }
+
+    fn from_str(text: &str) -> Vec<Self> {
+        text.chars()
+            .enumerate()
+            .map(|(i, ch)| Punctuation::from_char(i, ch, text.chars().nth(i + 1)))
+            .filter(Option::is_some)
+            .map(Option::unwrap)
+            .collect()
+    }
+}
 
 #[derive(Default)]
 pub struct Source {
@@ -41,8 +115,59 @@ impl Sentence {
         // verses.
         let indices = verses
             .iter()
-            .map(|verse| find_all_delimiters(verse, SENTENCE_DELIMITERS))
+            .map(|verse| Punctuation::from_str(verse))
+            .map(|verse_puncts| {
+                let mut indices = vec![];
+
+                // Sequence of adjacent punctuations led by a
+                // terminating one.
+                let mut state = vec![];
+
+                for punct in verse_puncts {
+                    let mut output = None;
+
+                    match punct.kind {
+                        PunctuationKind::Terminating {
+                            precedes_punctuation: true,
+                        } => {
+                            debug_assert!(state.is_empty());
+                            state.push(punct);
+                        }
+                        PunctuationKind::Terminating {
+                            precedes_punctuation: false,
+                        } => {
+                            debug_assert!(state.is_empty());
+                            output = Some(punct.index);
+                        }
+                        PunctuationKind::NonTerminating {
+                            precedes_whitespace: false,
+                            end_of_string: false,
+                        } => {
+                            if !state.is_empty() {
+                                debug_assert!(state[0].is_terminating());
+                                state.push(punct);
+                            }
+                        }
+                        PunctuationKind::NonTerminating { .. } => {
+                            if !state.is_empty() {
+                                debug_assert!(state[0].is_terminating());
+                                for (i, p) in state.iter().rev().enumerate() {
+                                    debug_assert_eq!(p.index, punct.index - (i + 1));
+                                }
+                                output = Some(punct.index);
+                                state.clear();
+                            }
+                        }
+                    }
+
+                    if let Some(output) = output {
+                        indices.push(output);
+                    }
+                }
+                indices
+            })
             .collect::<Vec<_>>();
+
         let queue = verses
             .iter()
             .enumerate()
@@ -102,12 +227,6 @@ pub unsafe fn sentences_from_verses(
         .for_each(|(i, sentence)| out_buffer[i].copy_from(sentence));
 }
 
-fn find_all_delimiters(text: &str, delimiters: &str) -> Vec<usize> {
-    text.match_indices(|c| delimiters.contains(c))
-        .map(|(index, _)| index)
-        .collect::<Vec<_>>()
-}
-
 /// Splits a verse into segments where sentences in the verse are
 /// terminated.
 fn split_verse(verse: usize, text: &str, indices: &[usize]) -> Vec<Segment> {
@@ -151,6 +270,8 @@ fn split_verse(verse: usize, text: &str, indices: &[usize]) -> Vec<Segment> {
 
 #[cfg(test)]
 mod test {
+    use super::Sentence;
+
     #[test]
     fn split_verse() {
         let text = "Mission: Impossible.";
@@ -158,5 +279,30 @@ mod test {
         let segments = super::split_verse(2, text, indices);
         assert_eq!(segments[0].text, "Mission:");
         assert_eq!(segments[1].text, "Impossible.");
+    }
+
+    #[test]
+    fn non_terminating_punctuations() {
+        // text from John 1:15-16 ESV
+        let verses = &[
+            "(John bore witness about him, and cried out, \"This was he of \
+             whom I said, 'He who comes after me ranks before me, because \
+             he was before me.'\")",
+            "For from his fullness we have all received, grace upon grace.",
+        ];
+        let expected = &[
+            "(John bore witness about him, and cried out, \"This was he of \
+             whom I said, 'He who comes after me ranks before me, because \
+             he was before me.'\")",
+            "For from his fullness we have all received, grace upon grace.",
+        ];
+
+        for (i, sentence) in Sentence::sentences_from_verses(verses)
+            .sentences
+            .iter()
+            .enumerate()
+        {
+            assert_eq!(sentence.text, expected[i]);
+        }
     }
 }
