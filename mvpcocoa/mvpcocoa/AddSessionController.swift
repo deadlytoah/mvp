166ddb799edf258c9d407d2eb2851617bde27475
Chris Shin <heesuk@gmail.com>
2018-10-21 22:07:38 +0900

    Fix error dialog when adding session fails.
    The error dialog would briefly show, then disappear and the whole app
    would crash.

--- mvpcocoa/mvpcocoa/AddSessionController.swift
+++ mvpcocoa/mvpcocoa/AddSessionController.swift
@@ -28,18 +28,18 @@ class AddSessionController: NSViewController {
 
         do {
             try session.create()
+
+            let sessionController = self.presenting as! SessionController
+            sessionController.sessionSelected(session: session)
+
+            self.dismiss(self)
         } catch {
             let alert = NSAlert()
             alert.alertStyle = .critical
             alert.messageText = "Failed to create a new session (\(error) error)"
             alert.beginSheetModal(for: self.view.window!)
         }
-
-        let sessionController = self.presenting as! SessionController
-        sessionController.sessionSelected(session: session)
-
-        self.dismiss(self)
-    }
+   }
 
     @IBAction func performCancel(_ sender: Any) {
         self.dismiss(self)
